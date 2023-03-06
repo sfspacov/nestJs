418b5c6d74398f9a36cf326599d0506835d2f5db
sfspacov <stephan_fs@hotmail.com>
2023-03-06 15:48:41 -0500

    argon to hash password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,17 +2,30 @@ import { Injectable } from "@nestjs/common";
 import { User, Bookmark } from "@prisma/client";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
+import * as argon from 'argon2'
 
 @Injectable({})
-export class AuthService
-{
-    constructor(private prisma: PrismaService){}
-    signin(){
+export class AuthService {
+    constructor(private prisma: PrismaService) { }
+    signin() {
         //this.prisma.user.findFirst()
-        return {msg: "Im signin"}
+        return { msg: "Im signin" }
     }
-    signup(body){
-        //this.prisma.user.create()
-        return {msg: "I'm signup"}
+    async signup(dto: AuthDto) {
+        const hash = await argon.hash(dto.password);
+        const user = await this.prisma.user.create(
+            {
+                data:
+                {
+                    email: dto.email,
+                    hash
+                },
+                select:
+                {
+                    id: true,
+                    createdAt: true
+                }
+            })
+        return user;
     }
 }
